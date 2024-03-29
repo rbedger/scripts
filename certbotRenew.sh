868571af1e28f7bf5a120118b66b0219d97ba584
Robert Edger <redger@athenahealth.com>
2024-03-29 01:21:18 -0500

    📜

--- certbotRenew.sh
+++ certbotRenew.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# certbot must renew on port 80, but wut if ur using port 80?
+# one solution is to utilize config file imports, toggling the proxy_pass directive.
+#
+# replaces the contents of a dynamically loaded nginx config file,
+# asks certbot to renew the certificate, and replaces the contents of the file
+
 echo "proxy_pass certbot;" > /etc/nginx/whichServer.conf
 systemctl reload nginx
 
